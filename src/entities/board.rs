bd5affc57cd04f632a67d9f92e86e00b5135f002
Joey Gibson <joey@joeygibson.com>
2020-04-25 01:40:37 -0400

    moved get_neighbors

--- src/entities/board.rs
+++ src/entities/board.rs
@@ -50,45 +50,51 @@ impl Board {
     }
 
     pub fn step(&mut self) {
-        for row_num in 0..self.cells.len() {
-            let &mut row = self.cells.get_mut(row_num).unwrap();
-            for col_num in 0..row.len() {
-                let cell = row.get_mut(col_num);
-                let neighbors = self.get_neighbors(row_num, col_num);
+        let local_cells = &mut self.cells;
 
-                // cell.step(neighbors);
+        for (ri, row) in local_cells.iter_mut().enumerate() {
+            for (ci, cell) in row.iter_mut().enumerate() {
+                let neighbors = get_neighbors(local_cells, self.rows, self.columns, ri, ci);
+
+                cell.step(neighbors);
+            }
+        }
+    }
+}
+
+fn get_neighbors(
+    cells: &Vec<Vec<Cell>>,
+    rows: i32,
+    columns: i32,
+    r: usize,
+    c: usize,
+) -> Vec<&Cell> {
+    let mut neighbors: Vec<&Cell> = vec![];
+
+    for i in (-1 as i32)..2 {
+        for j in (-1 as i32)..2 {
+            let mut new_r: i32 = r as i32 + i;
+            let mut new_c: i32 = c as i32 + j;
+
+            if new_r >= rows {
+                new_r = 0;
+            } else if new_r < 0 {
+                new_r = rows - 1;
+            }
+
+            if new_c >= columns {
+                new_c = 0;
+            } else if new_c < 0 {
+                new_c = columns - 1;
+            }
+
+            if new_r != (r as i32) && new_c != (c as i32) {
+                neighbors.push(&cells[new_r as usize][new_c as usize]);
             }
         }
     }
 
-    fn get_neighbors(&self, r: usize, c: usize) -> Vec<&Cell> {
-        let mut neighbors: Vec<&Cell> = vec![];
-
-        for i in (-1 as i32)..2 {
-            for j in (-1 as i32)..2 {
-                let mut new_r:i32 = r as i32 + i;
-                let mut new_c:i32 = c as i32 + i;
-
-                if new_r >= self.rows  {
-                    new_r = 0;
-                } else if new_r < 0 {
-                    new_r = self.rows  - 1;
-                }
-
-                if new_c >= self.columns  {
-                    new_c = 0;
-                } else if new_c < 0 {
-                    new_c = self.columns  - 1;
-                }
-
-                if new_r != (r as i32) && new_c != (c as i32) {
-                    neighbors.push(&self.cells[new_r as usize][new_c as usize]);
-                }
-            }
-        }
-
-        neighbors
-    }
+    neighbors
 }
 
 fn create_cells(rows: i32, columns: i32) -> Vec<Vec<Cell>> {
